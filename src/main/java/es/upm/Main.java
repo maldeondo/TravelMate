6cedd971b93ab7ec28206f363c1b0f18d3349e58
Robert Voong <rvoong1408@gmail.com>
2025-11-18 16:04:45 +0100

    gitignore y nada

--- src/main/java/es/upm/Main.java
+++ src/main/java/es/upm/Main.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Por hacer: Implementar la lógica principal de la aplicación aquí
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.nextLine());
     }
 }
