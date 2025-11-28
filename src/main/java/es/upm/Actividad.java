7e6ff4ce60a4b6687deaa306cdbe65ff344bcc02
Mario Aldeondo <ledge.locust3106@eagereverest.com>
2025-11-28 22:32:26 +0100

    make voidChars work on null + void pointers

--- src/main/java/es/upm/Actividad.java
+++ src/main/java/es/upm/Actividad.java
@@ -104,11 +104,9 @@ public class Actividad {
     }
 
     private static boolean voidChars(String data) {
-        boolean blank = false; char data_char;
+        boolean blank = true; char data_char;
 
         if (data != null && data != "") {
-            blank = true;
-
             for (int i = 0; i < data.length(); i++) {
                 data_char = data.charAt(i);
 
