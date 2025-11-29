3d4eba4d672d770919a5893c5b0f84cc7fd5db5f
Mario Aldeondo <ledge.locust3106@eagereverest.com>
2025-11-29 20:28:46 +0100

    enforce correct values, handle null pointers

--- src/main/java/es/upm/CatalogoActividades.java
+++ src/main/java/es/upm/CatalogoActividades.java
@@ -6,9 +6,7 @@ import java.io.FileWriter;
 import java.io.FileReader;
 
 public class CatalogoActividades {
-
-    // by doc
-    private final int maxActividades;
+    private int maxActividades; //should be final, but won't compile
     private int actActividades = 0;
 
     // array aproach, the data structure is not specified by doc
@@ -19,9 +17,12 @@ public class CatalogoActividades {
     public static final int ERROR_DEMASIADOS = 2;
 
     public CatalogoActividades(int maxActividades) {
-        this.maxActividades = maxActividades;
+        if (maxActividades > 0) {
+            this.maxActividades = maxActividades;
 
-        arrayActividades = new Actividad[maxActividades];
+            arrayActividades = new Actividad[maxActividades];
+        }
+        
     }
 
     public boolean actividadesCompletas() { return actActividades == maxActividades; }
@@ -29,12 +30,12 @@ public class CatalogoActividades {
     public int getNumActividades() { return actActividades; }
 
 
-    private static boolean validEntry(Actividad act) { return (act != null); }
+    private static boolean notNullEntry(Actividad act) { return (act != null); }
 
     public int agregarActividad(Actividad actividad) {
         int exitcode;
 
-        if (!validEntry(actividad)) exitcode = ERROR_ACTIVIDAD_NULL;
+        if (!notNullEntry(actividad)) exitcode = ERROR_ACTIVIDAD_NULL;
         else if (actividadesCompletas()) exitcode = ERROR_DEMASIADOS;
         else {
             arrayActividades[actActividades] = actividad;
@@ -50,52 +51,61 @@ public class CatalogoActividades {
         int target_index = -1;
         boolean target_found = false;
 
-        // find the target
-        for (int i = 0; i < actActividades; i++) {
-            if (arrayActividades[i] == seleccionada) {
-                target_index = i;
-                target_found = true;
+        if (!notNullEntry(seleccionada)) System.out.println("null");
+        else {
+            // find the target
+            for (int i = 0; i < actActividades; i++) {
+                if (arrayActividades[i] == seleccionada) {
+                    target_index = i;
+                    target_found = true;
+                }
             }
-        }
 
-        // replace each entry after the removed target
-        if (target_found) {
-            for (int i = target_index; i < actActividades - 1; i++) {
-                arrayActividades[i] = arrayActividades[i + 1];
-            }
-            actActividades--;
-        }    
-        
+            // replace each entry after the removed target
+            if (target_found) {
+                for (int i = target_index; i < actActividades - 1; i++) {
+                    arrayActividades[i] = arrayActividades[i + 1];
+                }
+                actActividades--;
+            }  
+        }
+  
         return target_found; 
     }
 
     public Actividad[] buscarActividadPorNombre(String texto) {
-        int[] index_array = new int[maxActividades];
-        texto = texto.toLowerCase();
-        
-        int target_count = 0;
 
-        // search for targets and save its index into the temp index_array
-        for (int i = 0; i < actActividades; i++) {
+        // create the void array before so to check if texto is a null pointer
+        Actividad[] target_array = {};
 
-            // check if the LOWERCASE name contains any LOWERCASE substring
-            // that matches with the provided texto
-            if (arrayActividades[i].getNombre().toLowerCase().contains(texto)) {
+        if (texto != null) {
+            int[] index_array = new int[maxActividades];
+            texto = texto.toLowerCase();
+            
+            int target_count = 0;
 
-                // index_array[i] = i would result in an array with "jumps"
-                // here target_count is recycled to get a continious array
-                index_array[target_count] = i;
-                target_count++;
+            // search for targets and save its index into the temp index_array
+            for (int i = 0; i < actActividades; i++) {
+
+                // check if the LOWERCASE name contains any LOWERCASE substring
+                // that matches with the provided texto
+                if (arrayActividades[i].getNombre().toLowerCase().contains(texto)) {
+
+                    // index_array[i] = i would result in an array with "jumps"
+                    // here target_count is recycled to get a continious array
+                    index_array[target_count] = i;
+                    target_count++;
+                }
             }
-        }
 
-        Actividad[] target_array = new Actividad[target_count];
+            target_array = new Actividad[target_count];
 
-        for (int i = 0; i < target_count; i++) {
+            for (int i = 0; i < target_count; i++) {
 
-            // build target_array with the main arrayActividades value ONLY using
-            // the correct indexes found previously (which are inside index_array)
-            target_array[i] = arrayActividades[index_array[i]];
+                // build target_array with the main arrayActividades value ONLY using
+                // the correct indexes found previously (which are inside index_array)
+                target_array[i] = arrayActividades[index_array[i]];
+            }
         }
 
         return target_array;
